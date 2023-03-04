cc5fb11651c4602dce83b38ff73ae008ff455dd2
ramazzzini <ramazan98derbent@gmail.com>
2023-03-04 21:45:23 +0500

    cards2

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+slidesPlugin(4)
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    })
-}
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
